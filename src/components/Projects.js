5680ba9b60d9d66cea7c7def19253aaf70bbdfa8
abdikot <abdiaryasa88@gmail.com>
2024-08-22 19:50:27 +0800

    Hari Ke-9 Animasi

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,110 +8,146 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(useGSAP);
 
 const Project = () => {
-
   const sectionRef = useRef(null);
-  const pRef = useRef(null);
   const textRef = useRef(null);
-  const buttonRef = useRef(null);
-  const spanRef = useRef(null);
+  const imgRefs = useRef([]);
+
+  imgRefs.current = [];
+
+  const addToImgRefs = (el) => {
+    if (el && !imgRefs.current.includes(el)) {
+      imgRefs.current.push(el);
+    }
+  };
 
   useGSAP(() => {
-    const tl1 = gsap.timeline({
-      scrollTrigger: {
-        trigger: sectionRef.current,
-        start: "top top",
-        end: "+=1000",
-        pin: true,
-        scrub: true
-      }
+    gsap.set(textRef.current, { y: 0 });
+
+    imgRefs.current.forEach((img, index) => {
+      const direction = index % 2 === 0 ? -100 : 100;
+      gsap.set(img, { opacity: 0, x: direction });
+      gsap.to(img,
+        {
+          opacity: 1,
+          x: 0, 
+          duration: 1,
+          ease: "power3.out",
+          scrollTrigger: {
+            trigger: img,
+            start: "top 80%", 
+            end: "bottom 60%",
+            scrub: true,
+          },
+          delay: index * 0.2 
+        }
+      );
     });
 
-    tl1.to([pRef.current, textRef.current, spanRef.current, buttonRef.current], {  opacity: 0, duration: 0.5 })
-       .to(spanRef.current, {  display: "block", opacity: 1, yPercent: -100,  duration: 1 })
+    ScrollTrigger.create({
+      trigger: sectionRef.current,
+      start: "top top",
+      end: "bottom bottom",
+      pin: textRef.current,
+      pinSpacing: false,
+      scrub: true,
+    });
+  }, { scope: sectionRef });
 
-    gsap.to(spanRef.current, 
-      {scrollTrigger: {
-        trigger: sectionRef.current,
-        start: "+=15 top",
-        end: "bottom top", 
-        pin: spanRef.current,
-        pinSpacing: false,
-        scrub: true,
-        markers: true
-    }})
-    
-  }, {scope: sectionRef})
   return (
     <section ref={sectionRef} className="relative h-auto py-10 md:py-0">
       <div className="h-auto md:min-h-screen text-custom-gray flex flex-col items-center justify-between py-10 md:py-32">
-        <p ref={pRef} className="text-center text-xl md:text-2xl leading-[29.05px]">
+        <p className="text-center text-xl md:text-2xl leading-[29.05px]">
           Our Project
         </p>
 
-        <h1 className="mt-10 md:mt-[150px] text-center text-xxl4 md:text-xxl10 px-4  lg:px-[350px] xl:px-[350px]">
-          <span ref={textRef}>Discover the Canvas of </span>
-          <span ref={spanRef} className="font-play-fair italic z-10">Our Creativity</span>
+        <h1 ref={textRef} className="mt-10 md:mt-[150px] text-center text-xxl4 md:text-xxl10 px-4 lg:px-[350px] xl:px-[350px] z-10">
+          Discover the Canvas of 
+          <span className="font-play-fair italic z-10">Our Creativity</span>
         </h1>
 
-        <button ref={buttonRef} className="bg-transparent h-[40px] w-[180px] md:h-[57px] md:w-[256px] border-custom-gray border-[1px] rounded-full text-base md:text-2xl mt-8">
+        <button className="bg-transparent h-[40px] w-[180px] md:h-[57px] md:w-[256px] border-custom-gray border-[1px] rounded-full text-base md:text-2xl mt-8">
           See all projects
         </button>
       </div>
 
       <div className="flex flex-col md:flex-row px-4 md:px-[50px] lg:px-[100px] mt-10 md:mt-0">
         <div className="flex flex-col w-full md:w-1/3 md:mr-[30px] lg:mr-[60px] xl:mr-[100px]">
-          <img
-            src="/asset/project-1.png"
-            className="w-full h-[300px] md:h-[450px] object-cover"
-          />
-          <img
-            src="/asset/project-4.png"
-            className="w-full h-[300px] md:h-[450px] object-cover mt-8 md:mt-32"
-          />
-          <img
-            src="/asset/project-7.png"
-            className="w-full h-[300px] md:h-[450px] object-cover mt-8 md:mt-32"
-          />
-          <img
-            src="/asset/project-10.png"
-            className="w-full h-[300px] md:h-[450px] object-cover mt-8 md:mt-32"
-          />
+          <div ref={addToImgRefs}>
+            <img
+              src="/asset/project-1.png"
+              className="w-full h-[300px] md:h-[450px] object-cover"
+            />
+          </div>
+          <div ref={addToImgRefs} className="mt-8 md:mt-32">
+            <img
+              src="/asset/project-4.png"
+              className="w-full h-[300px] md:h-[450px] object-cover"
+            />
+          </div>
+          <div ref={addToImgRefs} className="mt-8 md:mt-32">
+            <img
+              src="/asset/project-7.png"
+              className="w-full h-[300px] md:h-[450px] object-cover"
+            />
+          </div>
+          <div ref={addToImgRefs} className="mt-8 md:mt-32">
+            <img
+              src="/asset/project-10.png"
+              className="w-full h-[300px] md:h-[450px] object-cover"
+            />
+          </div>
         </div>
         <div className="flex flex-col w-full md:w-1/3 md:mr-[30px] lg:mr-[60px] xl:mr-[100px] mt-10 md:mt-12 lg:mt-[248px]">
-          <img
-            src="/asset/project-2.png"
-            className="w-full h-[300px] md:h-[450px] object-cover"
-          />
-          <img
-            src="/asset/project-5.png"
-            className="w-full h-[300px] md:h-[450px] object-cover mt-8 md:mt-12 lg:mt-32"
-          />
-          <img
-            src="/asset/project-8.png"
-            className="w-full h-[300px] md:h-[450px] object-cover mt-8 md:mt-12 lg:mt-32"
-          />
-          <img
-            src="/asset/project-11.png"
-            className="w-full h-[300px] md:h-[450px] object-cover mt-8 md:mt-12 lg:mt-32"
-          />
+          <div ref={addToImgRefs}>
+            <img
+              src="/asset/project-2.png"
+              className="w-full h-[300px] md:h-[450px] object-cover"
+            />
+          </div>
+          <div ref={addToImgRefs} className="mt-8 md:mt-12 lg:mt-32">
+            <img
+              src="/asset/project-5.png"
+              className="w-full h-[300px] md:h-[450px] object-cover"
+            />
+          </div>
+          <div ref={addToImgRefs} className="mt-8 md:mt-12 lg:mt-32">
+            <img
+              src="/asset/project-8.png"
+              className="w-full h-[300px] md:h-[450px] object-cover"
+            />
+          </div>
+          <div ref={addToImgRefs} className="mt-8 md:mt-12 lg:mt-32">
+            <img
+              src="/asset/project-11.png"
+              className="w-full h-[300px] md:h-[450px] object-cover"
+            />
+          </div>
         </div>
         <div className="flex flex-col w-full md:w-1/3 mt-10 md:mt-0 lg:mt-32">
-          <img
-            src="/asset/project-3.png"
-            className="w-full h-[300px] md:h-[450px] object-cover"
-          />
-          <img
-            src="/asset/project-6.png"
-            className="w-full h-[300px] md:h-[450px] object-cover mt-8 md:mt-12 lg:mt-32"
-          />
-          <img
-            src="/asset/project-9.png"
-            className="w-full h-[300px] md:h-[450px] object-cover mt-8 md:mt-12 lg:mt-32"
-          />
-          <img
-            src="/asset/project-12.png"
-            className="w-full h-[300px] md:h-[450px] object-cover mt-8 md:mt-12 lg:mt-32"
-          />
+          <div ref={addToImgRefs}>
+            <img
+              src="/asset/project-3.png"
+              className="w-full h-[300px] md:h-[450px] object-cover"
+            />
+          </div>
+          <div ref={addToImgRefs} className="mt-8 md:mt-12 lg:mt-32">
+            <img
+              src="/asset/project-6.png"
+              className="w-full h-[300px] md:h-[450px] object-cover"
+            />
+          </div>
+          <div ref={addToImgRefs} className="mt-8 md:mt-12 lg:mt-32">
+            <img
+              src="/asset/project-9.png"
+              className="w-full h-[300px] md:h-[450px] object-cover"
+            />
+          </div>
+          <div ref={addToImgRefs} className="mt-8 md:mt-12 lg:mt-32">
+            <img
+              src="/asset/project-12.png"
+              className="w-full h-[300px] md:h-[450px] object-cover"
+            />
+          </div>
         </div>
       </div>
     </section>
